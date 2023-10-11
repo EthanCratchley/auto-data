c4db1641c3a4965d91a6b527a8ee4af45d9ae7e5
Ethan <ethankcratchley@gmail.com>
2023-10-11 08:38:18 -0700

    Added Documentation, finished cleaning, moved to TSLA folder. Project is complete.
    May return to expand in the future.

--- auto_clean.py
+++ auto_clean.py
@@ -1,45 +1,49 @@
+# Importing necessary libraries and modules
 from auto_scrape import fetch_and_extract
 import pandas as pd
 import numpy as np
 
-# Specify URL and table class name
+# URL of the webpage containing the table to be scraped
 url = 'https://www.wsj.com/market-data/quotes/TSLA/financials/annual/balance-sheet'
+
+# The class name of the table to be scraped
 table_class = 'cr_dataTable'
 
-# Fetch and extract data
+# Call the fetch_and_extract function to scrape data from the given URL and table class
 data = fetch_and_extract(url, table_class)
 
+# Check if data was successfully scraped
 if data is not None:
-    # Clean the data if needed
-    # For example, remove NaN values, convert data types, etc.
-
-    # Save to CSV
+    # If data is found, save it as a CSV file
     data.to_csv('data_assets.csv', index=False)
     print("Data saved to data.csv")
 else:
+    # If no data is found, print a message
     print("No data to clean and save.")
 
-
+# Function to clean the scraped data
 def clean_data(file_name, rows_to_drop):
     try:
+        # Load the CSV file into a DataFrame
         df = pd.read_csv(file_name)
         print(f"Original DataFrame:\n{df}\n")
 
-        # Replace '-' with NaN
+        # Replace '-' characters with NaN to allow for dropping them later
         df.replace('-', np.nan, inplace=True)
 
-        # Drop specified rows
+        # Drop specific rows based on the provided indices
         df.drop(index=rows_to_drop, inplace=True)
 
-        # Drop columns beyond the 5th index
+        # Drop columns beyond the 5th index to clean up the DataFrame
         df.drop(columns=df.columns[6:], inplace=True)
 
-        # Drop any remaining rows where all values are NaN
+        # Drop rows where all values are NaN
         df.dropna(how='all', inplace=True)
 
-        # Optional: Drop any remaining rows where all values are NaN across a specific set of columns
+        # Optionally, drop rows where all values in specific columns are NaN
         df.dropna(subset=df.columns[1:], how='all', inplace=True)
 
+        # Print the cleaned DataFrame
         print(f"Cleaned DataFrame:\n{df}\n")
         return df
 
@@ -48,16 +52,15 @@ def clean_data(file_name, rows_to_drop):
     except Exception as e:
         print(f"An error occurred while processing {file_name}: {e}")
 
+# Indices of rows to be dropped for assets and liabilities
+rows_to_drop_assets = [1,4,10,11,18,20,26,29,32,35,36,37,38,39,40, 41,42,43,45,52,56]
+rows_to_drop_liab = [8,22,27,29,33,35,36,37,38,39,44,45,46,47, 49,50]
 
-# Specify the rows to drop for each dataset
-rows_to_drop_assets = [1,4,10,11,18,20,26,29,32,35,36,37,38,39,40,
-                       41,42,43,45,52,56]
-rows_to_drop_liab = [8,22,27,29,33,35,36,37,38,39,44,45,46,47,
-                     49,50]
-
+# Cleaning the data for assets and liabilities
 cleaned_assets = clean_data('data_assets.csv', rows_to_drop_assets)
 cleaned_liab = clean_data('data_liab.csv', rows_to_drop_liab)
 
+# Save the cleaned data to new CSV files
 if cleaned_assets is not None:
     cleaned_assets.to_csv('cleaned_assets.csv', index=False)
 
--- auto_scrape.py
+++ auto_scrape.py
@@ -1,32 +1,53 @@
+# Import necessary libraries
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# Function to fetch and extract a table from a webpage
 def fetch_and_extract(url, table_class):
+    # Headers to mimic a real browser visit to the site, which can help avoid being blocked
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
+
+    # Send a GET request to the specified URL with the headers
     page = requests.get(url, headers=headers)
+
+    # Initialize the data variable to None. It will hold the extracted table data if found
     data = None
 
+    # Check if the GET request was successful (status code 200)
     if page.status_code == 200:
+        # Parse the HTML content of the page with Beautiful Soup
         soup = BeautifulSoup(page.text, 'html.parser')
+
+        # Search for all tables with the specified class name
         table = soup.find_all('table', {'class': table_class})
 
+        # If a table is found
         if table:
+            # Convert the table HTML into a pandas DataFrame
             data = pd.read_html(str(table))[0]
+
+            # Print confirmation and display the head of the DataFrame
             print("Table found!")
             print(data.head())
+
         else:
+            # Print a message if no table with the specified class name is found
             print("No table found with the specified class name.")
+
     else:
+        # Print an error message if the GET request was not successful
         print(f"Failed to retrieve content. Status code: {page.status_code}")
 
+    # Return the extracted table data as a pandas DataFrame, or None if not found
     return data
 
-# Test the function with your specific URL and table class name
+# Test the function with a specific URL and table class name
+# This URL and class name should be updated to target different webpages/tables as needed
 url = 'https://www.wsj.com/market-data/quotes/TSLA/financials/annual/balance-sheet'
 table_class = 'cr_dataTable'
+
+# Call the fetch_and_extract function and store the extracted data in the 'data' variable
 data = fetch_and_extract(url, table_class)
-
-
